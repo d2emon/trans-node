edd935b5642c63b08e39d7efad03e94c3ab0519f
Dmitry Kutsenko <d2emonium@gmail.com>
2023-01-25 09:53:24 +0300

    Fix eslint errors

--- front/src/services/transports.js
+++ front/src/services/transports.js
@@ -1,33 +1,35 @@
-import routesService, { bus, hover, monorail, tube } from "./routes";
+import routesService, {
+  bus, hover, monorail, tube,
+} from './routes';
 
 const getTransport = async (cityId, name, slug) => ({
-    name,
-    slug,
-    cityId,
+  name,
+  slug,
+  cityId,
 });
 
 const load = async () => Promise.all([
-    getTransport('nowhere', 'Монорельсы', monorail),
-    getTransport('nowhere', 'Ховеры', hover),
-    getTransport('nowhere', 'Автобусы', bus),
-    getTransport('nowhere', 'Метро', tube),
+  getTransport('nowhere', 'Монорельсы', monorail),
+  getTransport('nowhere', 'Ховеры', hover),
+  getTransport('nowhere', 'Автобусы', bus),
+  getTransport('nowhere', 'Метро', tube),
 ]);
 
 const byCityId = async (cityId) => {
-    const items = await load();
-    const transport = items
-        .filter(item => item.cityId === cityId)
-        .map(item => async () => {
-            const routes = await routesService.byTransportId(cityId, item.slug);
-            return {
-                ...item,
-                routes,
-            }
-        });
-    return Promise.all(transport.map(loader => loader()));
-}
+  const items = await load();
+  const transport = items
+    .filter((item) => item.cityId === cityId)
+    .map((item) => async () => {
+      const routes = await routesService.byTransportId(cityId, item.slug);
+      return {
+        ...item,
+        routes,
+      };
+    });
+  return Promise.all(transport.map((loader) => loader()));
+};
 
 export default {
-    byCityId,
-    load,
+  byCityId,
+  load,
 };
--- front/src/store.js
+++ front/src/store.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import breadcrumbsReducer from './reducers/breadcrumbsSlice';
 import counterReducer from './reducers/counterSlice';
 import routeReducer from './reducers/routeSlice';
+import userReducer from './reducers/userSlice';
 
 const store = configureStore({
   reducer: {
     counter: counterReducer,
     breadcrumbs: breadcrumbsReducer,
     routes: routeReducer,
+    user: userReducer,
   },
 });
 
